33d645f91204e38abd7c8df356b8f41d11a25594
Scott Walton <scott.walton@mypebble.co.uk>
2016-09-30 15:44:59 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 from setuptools import setup, find_packages
 
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Framework :: Django',
+]
+
+
 setup(
     name='queue-fetcher',
-    version='0.0.1',
+    version='1.0.0',
     description="QueueFetcher makes dealing with SQS queues in Django easier",
     author="SF Software limited t/a Pebble",
     author_email="sysadmin@mypebble.co.uk",
@@ -10,5 +24,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['six']
+    install_requires=['six'],
+    classifiers=CLASSIFIERS,
 )
