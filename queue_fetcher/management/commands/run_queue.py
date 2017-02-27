1055826fc9482bc639f8260007bb64171bea739d
Scott Walton <scott.walton@mypebble.co.uk>
2017-02-27 14:15:49 +0000

    Bump version with update for newer Django versions

--- queue_fetcher/management/commands/run_queue.py
+++ queue_fetcher/management/commands/run_queue.py
@@ -1,4 +1,4 @@
-"""Run the selected queue task
+"""Run the selected queue task.
 """
 from django.utils.module_loading import import_string
 from django.core.management.base import BaseCommand
@@ -6,22 +6,26 @@ from django.apps import apps
 
 
 class Command(BaseCommand):
+    """Connect to the SQS queue on our behalf.
     """
-    """
-    args = '<task>'
-    help = (
-        'Run the selected queue task'
-    )
+
+    help = ('Run the selected queue task')
+
+    def add_arguments(self, parser):
+        """Add the required task argument.
+        """
+        parser.add_argument('task', type=str, help='Task to run')
 
     def get_app_module(self, app_name):
+        """Return the app module containing the task.
+        """
         config = apps.get_app_config(app_name)
         return config.module
 
-    def handle(self, task, *args, **kwargs):
-        """
+    def handle(self, task, *args, **kwargs):  # pylint: disable=W0613
+        """Handle the run_queue command.
         """
         app_label, task = task.split('.')
         task = import_string('{}.tasks.{}'.format(
             self.get_app_module(app_label).__name__, task))
-        t = task()
-        t.run()
+        task().run()
