4f38a44aca504d252f3b832b797c7183c33c5104
Tom Picton <tom@tompicton.co.uk>
2018-01-16 13:59:54 +0000

    Fix test failure

--- test_project/test_project/qf_test/tests/test_sqs.py
+++ test_project/test_project/qf_test/tests/test_sqs.py
@@ -1,4 +1,4 @@
-from mock import patch, MagicMock
+from mock import patch
 
 from django.test import TestCase, override_settings
 
@@ -8,15 +8,52 @@ from queue_fetcher.utils import sqs
 class SQSTestCase(TestCase):
     """Test SQS works in the test mode correctly
     """
+
+    def setUp(self):
+        """Clear the text outbox.
+        """
+        sqs.clear_outbox()
+
     def test_sqs(self):
         # Get queue
         queue = sqs.get_queue('test')
-        self.assertEquals(queue.name, 'test')
-        # Send a message
+        self.assertEquals(queue.name, 'test')  # Send a message
         sqs.send_message(queue, {
             'message_type': 'demo',
             'this': 'should work'
         })
+        # It should drop into the outbox
+        self.assertEquals(len(sqs.outbox['test']), 1)
+        self.assertEquals(sqs.outbox['test'][0], {
+            'message_type': 'demo',
+            'this': 'should work'
+        })
+
+    def test_sqs_string(self):
+        """Test the send_message method with a string.
+        """
+        # Get queue
+        queue = sqs.get_queue('test')
+        self.assertEquals(queue.name, 'test')
+        # Send a message
+        sqs.send_message(
+            queue, '{"message_type": "demo","this": "should work"}')
+        # It should drop into the outbox
+        self.assertEquals(len(sqs.outbox['test']), 1)
+        self.assertEquals(sqs.outbox['test'][0], {
+            'message_type': 'demo',
+            'this': 'should work'
+        })
+
+    def test_sqs_binary(self):
+        """Test the send_message method with a binary string.
+        """
+        # Get queue
+        queue = sqs.get_queue('test')
+        self.assertEquals(queue.name, 'test')
+        # Send a message
+        sqs.send_message(
+            queue, b'{"message_type": "demo","this": "should work"}')
         # It should drop into the outbox
         self.assertEquals(len(sqs.outbox['test']), 1)
         self.assertEquals(sqs.outbox['test'][0], {
@@ -31,4 +68,5 @@ class SQSTestCase(TestCase):
         """
         sqs.get_queue('arn:aws:sqs:nowhere:4444455556666:queuenamehere')
         resource.assert_called_with('sqs', region_name='nowhere')
-        resource.return_value.get_queue_by_name.assert_called_with(QueueName='queuenamehere', QueueOwnerAWSAccountId='4444455556666')
+        resource.return_value.get_queue_by_name.assert_called_with(
+            QueueName='queuenamehere', QueueOwnerAWSAccountId='4444455556666')
